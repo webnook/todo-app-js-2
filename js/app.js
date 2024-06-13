b481a03a6c0291ecbcd1ad54ec1be862e009c8ed
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-06-13 16:44:48 +0330

    alert-message created

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addButton = document.getElementById("add-button");
+const alertMessage = document.getElementById("alert-message");
+
+const todos = [];
+
+const showAlert = (message, type) => {
+  alertMessage.innerHTML = "";
+  const alert = document.createElement("p");
+  alert.innerText = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
+
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+  const todo = {
+    task,
+    date,
+    completed: false,
+  };
+  if (task) {
+    todos.push(todo);
+    taskInput.value = "";
+    dateInput.value = "";
+    showAlert("Todo added successfully", "success");
+  } else {
+    showAlert("Please enter a valid todo", "error");
+  }
+};
+
+addButton.addEventListener("click", addHandler);
